1ce7ad1f735943b4797f72a028684a4335faeb2e
DjDeveloper <denis.polulyakh.java@gmail.com>
2017-01-12 00:44:18 +0400

    new version

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var intents = new builder.IntentDialog();
+//var intents = new builder.IntentDialog();
 
 
 //=========================================================
@@ -26,236 +26,105 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 
 
-bot.dialog('/', intents);
+//bot.dialog('/', intents);
 
 
-intents.matches(/^список валют/i, [
+//intents.matches(/./g, [
+//  function (session) {
+//      session.beginDialog('/request');
+//   }
+//]);
+bot.dialog('/', [
     function (session) {
-        session.beginDialog('/listcurrency');
-
-    }
-
-]);
-
-
-intents.matches(/^курс USD/i, [
-    function (session) {
-        session.beginDialog('/profileUSD');
-
-    }
-
-]);
-
-intents.matches(/^курс доллара/i, [
-    function (session) {
-        session.beginDialog('/profileUSD');
-
-    }
-
-]);
-intents.matches(/^курс бакса/i, [
-    function (session) {
-        session.beginDialog('/profileUSD');
-
-    }
-
-]);
-intents.matches(/^курс бакинского/i, [
-    function (session) {
-        session.beginDialog('/profileUSD');
+        session.beginDialog('/request');
     }
 ]);
 
-intents.matches(/^курс EUR/i, [
-    function (session) {
-        session.beginDialog('/profileEUR');
+// bot.dialog('/profileUSD', [
+//     function (session) {
+//         var request = require('request');
+//         var dateFormat = require('dateformat');
+//         var date = dateFormat(new Date, "dd.mm.yyyy");
+//         request.get('https://frankbotmind.herokuapp.com/exchange?name=USD&date=' + date, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//                 phrase = JSON.parse(body);
+//                 console.log(phrase);
+//                 session.send(phrase.phrase);
+//             }
+//         })
+//         session.endDialog();
+//     }
+// ]);
 
+// bot.dialog('/profileEUR', [
+//     function (session) {
+//         var request = require('request');
+//         var dateFormat = require('dateformat');
+//         var date = dateFormat(new Date, "dd.mm.yyyy");
+//         request.get('https://frankbotmind.herokuapp.com/exchange?name=EUR&date=' + date, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//                 phrase = JSON.parse(body);
+//                 console.log(phrase);
+//                 session.send(phrase.phrase);
+//             }
+//         })
+//         session.endDialog();
+//     }
+// ]);
+
+// bot.dialog('/profileUAH', [
+//     function (session) {
+//         var request = require('request');
+//         var dateFormat = require('dateformat');
+//         var date = dateFormat(new Date, "dd.mm.yyyy");
+//         request.get('https://frankbotmind.herokuapp.com/exchange?name=UAH&date=' + date, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//                 phrase = JSON.parse(body);
+//                 console.log(phrase);
+//                 session.send(phrase.phrase);
+//             }
+//         })
+//         session.endDialog();
+//     }
+// ]);
+
+function getJSONProperty(bodyJson, property) {
+
+    console.log("Body: " + bodyJson);
+    console.log("Property: " + property);
+    body = JSON.parse(bodyJson);
+    if (property == 'phrase') {
+        return body.phrase;
     }
+}
 
-]);
-
-intents.matches(/^курс евро/i, [
-    function (session) {
-        session.beginDialog('/profileEUR');
-
+function botAnswer(session, phrases) {
+    console.log("Phrases: " + phrases);
+    for (i = 0; i < phrases.length; i++) {
+        session.send(phrases[i]);
     }
-
-]);
-intents.matches(/^курс еврика/i, [
-    function (session) {
-        session.beginDialog('/profileEUR');
-
-    }
-
-]);
-intents.matches(/^курс Евро/i, [
-    function (session) {
-        session.beginDialog('/profileEUR');
-    }
-]);
-
-intents.matches(/^курс UAH/i, [
-    function (session) {
-        session.beginDialog('/profileUAH');
-
-    }
-
-]);
-
-intents.matches(/^курс гривны/i, [
-    function (session) {
-        session.beginDialog('/profileUAH');
-
-    }
-
-]);
-
-intents.matches(/^курс Гривны/i, [
-    function (session) {
-        session.beginDialog('/profileUAH');
-
-    }
-
-]);
-intents.matches(/^Слава Украiне!/i, [
-    function (session) {
-        session.beginDialog('/profileUAH');
-
-    }
-
-]);
-
-intents.matches(/^Привет, Фрэнк/i, [
-    function (session) {
-        session.beginDialog('/hello');
-    }
-
-]);
-
-intents.matches(/^привет бот/i, [
-    function (session) {
-        session.beginDialog('/hellosimple');
-    }
-
-]);
+    return body.property;
+}
 
 
-intents.matches(/^Фрэнк, как дела\?/, [
-    function (session) {
-         session.beginDialog('/howareyou');
-  }
-
-]);
-
-intents.matches(/^хуй пизда/i, [
-    function (session) {
-         session.beginDialog('/pussy');
-  }
-]);
-
-intents.matches(/^хуй/i, [
-    function (session) {
-         session.beginDialog('/pussy');
-  }
-]);
-
-bot.dialog('/profileUSD', [
+bot.dialog('/request', [
     function (session) {
         var request = require('request');
-        var dateFormat = require('dateformat');
-        var date = dateFormat(new Date, "dd.mm.yyyy");
-        request.get('https://frankbotmind.herokuapp.com/exchange?name=USD&date=' + date, function (error, response, body) {
+        var message = JSON.stringify({ text: encodeURIComponent(session.message.text) });
+        var address = 'https://cleverfrankbotmind.herokuapp.com/?message=' + message;
+        console.log(address);
+        request.get(address, function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                phrase = JSON.parse(body);
-                console.log(phrase);
-                session.send(phrase.phrase);
+                var phrases = getJSONProperty(body, 'phrase');
+                console.log("Phrases: " + phrases);
+                botAnswer(session, phrases);
+            } else {
+                session.send("Простите, я не могу сейчас разговаривать");
             }
-        })
-        session.endDialog();
-    }
-]);
-
-bot.dialog('/profileEUR', [
-    function (session) {
-        var request = require('request');
-        var dateFormat = require('dateformat');
-        var date = dateFormat(new Date, "dd.mm.yyyy");
-        request.get('https://frankbotmind.herokuapp.com/exchange?name=EUR&date=' + date, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                phrase = JSON.parse(body);
-                console.log(phrase);
-                session.send(phrase.phrase);
-            }
-        })
-        session.endDialog();
-    }
-]);
-
-bot.dialog('/profileUAH', [
-    function (session) {
-        var request = require('request');
-        var dateFormat = require('dateformat');
-        var date = dateFormat(new Date, "dd.mm.yyyy");
-        request.get('https://frankbotmind.herokuapp.com/exchange?name=UAH&date=' + date, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                phrase = JSON.parse(body);
-                console.log(phrase);
-                session.send(phrase.phrase);
-            }
-        })
-        session.endDialog();
-    }
-]);
-
-bot.dialog('/listcurrency', [
-    function (session) {
-        var request = require('request');
-        var dateFormat = require('dateformat');
-        var date = dateFormat(new Date, "dd.mm.yyyy");
-        request.get('https://frankbotmind.herokuapp.com/listcurrency', function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                phrase = JSON.parse(body);
-                console.log(phrase);
-            }
-        })
+        });
         session.endDialog();
     }
 ]);
 
 
 
-bot.dialog('/hello', [
-    function (session) {
-      session.send("Приветствую!!! Меня зовут Фрэнк Каупервуд. Я могу сказать Вам текущий курс Евро, Доллара, Гривны. Просто наберите: \"курс [валюта]\"");
-      session.endDialog();
-    }
-
-      
-]);
-
-bot.dialog('/pussy', [
-    function (session) {
-      session.send("Смотря в каком контексте здесь пизда :)");
-      session.endDialog();
-    }
-
-      
-]);
-
-bot.dialog('/howareyou', [
-    function (session) {
-      session.send("У меня хорошо, а ты работай, а не с ботом пизди!!!");
-      session.endDialog();
-    }
-
-      
-]);
-
-bot.dialog('/hellosimple', [
-    function (session) {
-      session.send("Привет!");
-      session.endDialog();
-    }
-
-      
-]);
