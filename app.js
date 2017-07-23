c7114d2558be2906295e7ede07cc2609ceedaab0
Denis Polulyakh <rich.hcir.error@gmail.com>
2017-07-23 14:31:21 +0400

    search

--- app.js
+++ app.js
@@ -136,8 +136,9 @@ bot.dialog('/request', [
         console.log(text);
         var messageToService = JSON.stringify({ text: text,timestamp:session.message.timestamp,address:session.message.address });
         console.log(messageToService);
-        var address = 'https://cleverfrankbotmind.herokuapp.com/botmind?message=' + encodeURIComponent(messageToService);
+        //var address = 'https://cleverfrankbotmind.herokuapp.com/botmind?message=' + encodeURIComponent(messageToService);
         //var address ='https://neznayka-front-controller.herokuapp.com/getAnswer?message=' + encodeURIComponent(messageToService);
+        var address = 'https://cleverfrankbotmind.herokuapp.com/search?message=' + encodeURIComponent(messageToService);
         console.log(address);
         request.get(address, function (error, response, body) {
             if (!error && response.statusCode == 200) {
