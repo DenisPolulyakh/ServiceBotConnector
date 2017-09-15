1fe57a564ad342058ec0ae3de2a94ae7db8e2a79
Denis Polulyakh <rich.hcir.error@gmail.com>
2017-09-15 07:19:19 +0400

    console log

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ function botAnswer(session, phrases) {
     console.log("Phrases: " + phrases);
     for (i = 0; i < phrases.length; i++) {
         var m = {"text":"Qwerty","timestamp":"2017-09-15T03:01:16.9633025Z","address":{"id":"16fa840be3ea4073a0a33651d2ad0c64|0000000","channelId":"webchat","user":{"id":"H5QnNQnETbb","name":"You"},"conversation":{"id":"16fa840be3ea4073a0a33651d2ad0c64"},"bot":{"id":"frankbotskype@9egOKufsX9g","name":"FrankCowperwood"},"serviceUrl":"https://webchat.botframework.com","useAuth":true}};
-        session.log("SEND "+phrases[i]);
+        console.log("SEND "+phrases[i]);
         session.send(phrases[i]);
     }
     return body.property;
