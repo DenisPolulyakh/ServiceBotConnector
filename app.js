63e7eeba0efe71b73d90dfed8c4947cfeab5305f
Denis Polulyakh <rich.hcir.error@gmail.com>
2017-09-15 07:07:45 +0400

    check json

--- app.js
+++ app.js
@@ -101,7 +101,8 @@ function getJSONProperty(bodyJson, property) {
 function botAnswer(session, phrases) {
     console.log("Phrases: " + phrases);
     for (i = 0; i < phrases.length; i++) {
-        session.send(phrases[i]);
+        var m = {"text":"Qwerty","timestamp":"2017-09-15T03:01:16.9633025Z","address":{"id":"16fa840be3ea4073a0a33651d2ad0c64|0000000","channelId":"webchat","user":{"id":"H5QnNQnETbb","name":"You"},"conversation":{"id":"16fa840be3ea4073a0a33651d2ad0c64"},"bot":{"id":"frankbotskype@9egOKufsX9g","name":"FrankCowperwood"},"serviceUrl":"https://webchat.botframework.com","useAuth":true}};
+        session.send(m);
     }
     return body.property;
 }
